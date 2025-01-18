714d1e01e07652e710d2f75418829962600cba27
Nikita Noskov <nik030205@mail.ru>
2025-01-18 13:58:23 +0300

    update shortlink_redirect

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -8,5 +8,6 @@ from .models import Recipes
 def recipe_redirect(request, link):
     recipe = get_object_or_404(Recipes, short_link=link)
     recipe_url = reverse('recipe-detail', kwargs={'pk': recipe.id})
-
-    return HttpResponsePermanentRedirect(recipe_url)
+    print(recipe_url)
+    print(recipe_url[4:])
+    return HttpResponsePermanentRedirect(recipe_url[4:])
