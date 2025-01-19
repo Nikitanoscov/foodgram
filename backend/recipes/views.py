8adf7f8c2fceda203c00fec661020ea072356e60
Nikita Noskov <nik030205@mail.ru>
2025-01-19 13:48:11 +0300

    update

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -6,5 +6,5 @@ from .models import Recipes
 
 def recipe_redirect(request, link):
     recipe = get_object_or_404(Recipes, short_link=link)
-    recipe_url = request.build_absolute_uri(f'recipes/{recipe.id}/')
+    recipe_url = f'/recipes/{recipe.id}/'
     return HttpResponsePermanentRedirect(recipe_url)
